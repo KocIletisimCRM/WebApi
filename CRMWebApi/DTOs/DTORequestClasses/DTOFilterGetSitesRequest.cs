aee96a65b9d5904d609fe461d7eaf405fa6676bd
kociletisim <yazilim@kociletisim.com.tr>
2015-08-20 11:01:59 +0300

    .

---
+++ CRMWebApi/DTOs/DTORequestClasses/DTOFilterGetSitesRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMWebApi.DTOs.DTORequestClasses
+{
+    public class DTOFilterGetSitesRequest :DTOFilterRequestBase
+    {
+       
+    }
+}
