969d3f94c320bab2a89677e5cefafd61a2c21755
hsynkz <hsyn.kz.61@gmail.com>
2016-03-31 13:44:53 +0200

    SLID Filter update

--- CRMWebApi/DTOs/Adsl/DTORequestClasses/DTOFilterGetSLRequest.cs
+++ CRMWebApi/DTOs/Adsl/DTORequestClasses/DTOFilterGetSLRequest.cs
@@ -18,6 +18,7 @@ namespace CRMWebApi.DTOs.Adsl.DTORequestClasses
         public DTOFilter getFilter()
         {
             var filter = new DTOFilter("SL", "SLID");
+            if (SLID != null) filter.fieldFilters.Add(SLID);
             if (SLName != null) filter.fieldFilters.Add(SLName);
             if (KocSTask != null) filter.fieldFilters.Add(KocSTask);
             if (KocETask != null) filter.fieldFilters.Add(KocETask);
