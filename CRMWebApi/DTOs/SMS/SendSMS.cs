21a2706775433318f2c777c607dcb930834b325a
hsynkz <hsyn.kz.61@gmail.com>
2017-05-11 16:59:08 +0300

    VC

---
+++ CRMWebApi/DTOs/SMS/SendSMS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace CRMWebApi.DTOs.SMS
+{
+    public class SendSMS
+    {
+        //public 
+    }
+
+    public class QueryResponse
+    {
+        public int MessageId { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public class Status
+    {
+        public int Code { get; set; }
+        public string Description { get; set; }
+    }
+}
